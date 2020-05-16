0baa406ec424810cd9276d269b0eefe8dd3f99af
Aurelien Gateau <mail@agateau.com>
2020-05-16 15:29:38 +0200

    Add concept of player row

--- ctall/player.py
+++ ctall/player.py
@@ -2,31 +2,40 @@ import arcade
 
 SPRITE_SCALING = 1
 
+MIN_ROW = -2
+MAX_ROW = 2
+
+ROW_HEIGHT = 48
+MOVE_SPEED = 400
+
 
 class Player(arcade.Sprite):
-    MOVE_SPEED = 200
-
     def __init__(self, game):
         super().__init__("./images/player.png", SPRITE_SCALING)
-        self.velocity = [0, 0]
         self.game = game
+        self.current_row = 0
+        self.target_row = 0
+        self.left = 12
+        self.center_y = self.y_for_row(self.current_row)
 
     def update(self, delta):
         keys = self.game.keys
 
-        if keys[arcade.key.LEFT]:
-            self.velocity[0] = int(-self.MOVE_SPEED * delta)
-        elif keys[arcade.key.RIGHT]:
-            self.velocity[0] = int(self.MOVE_SPEED * delta)
-        else:
-            self.velocity[0] = 0
+        if keys[arcade.key.UP] and self.target_row < MAX_ROW \
+                and self.target_row == self.current_row:
+            self.target_row += 1
+        elif keys[arcade.key.DOWN] and self.target_row > MIN_ROW \
+                and self.target_row == self.current_row:
+            self.target_row -= 1
 
-        if keys[arcade.key.UP]:
-            self.velocity[1] = int(self.MOVE_SPEED * delta)
-        elif keys[arcade.key.DOWN]:
-            self.velocity[1] = int(-self.MOVE_SPEED * delta)
-        else:
-            self.velocity[1] = 0
+        target_y = self.y_for_row(self.target_row)
+        if self.target_row < self.current_row:
+            self.center_y = max(target_y, self.center_y - MOVE_SPEED * delta)
+        elif self.target_row > self.current_row:
+            self.center_y = min(target_y, self.center_y + MOVE_SPEED * delta)
 
-        self.center_x += self.velocity[0]
-        self.center_y += self.velocity[1]
+        if self.center_y == target_y:
+            self.current_row = self.target_row
+
+    def y_for_row(self, row):
+        return self.game.height / 2 + row * ROW_HEIGHT
