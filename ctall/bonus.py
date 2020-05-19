f1189ba42eddd584626d5d24f5135dcf5dc42211
Aurelien Gateau <mail@agateau.com>
2020-05-19 09:16:38 +0200

    Add support for multiple bonus images

--- ctall/bonus.py
+++ ctall/bonus.py
@@ -1,16 +1,38 @@
+import random
+
+from pathlib import Path
+
 import arcade
 
 from ctall.scrollcomponent import ScrollComponent
 from ctall.poolable import Poolable
 
 
+BONUS_TEXTURES = []
+
+
+def _init_bonus_textures():
+    global BONUS_TEXTURES
+    if BONUS_TEXTURES:
+        return
+
+    for path in Path("images/bonus").glob("*.png"):
+        BONUS_TEXTURES.append(arcade.load_texture(path))
+
+
+def pick_random_texture():
+    _init_bonus_textures()
+    return random.choice(BONUS_TEXTURES)
+
+
 class Bonus(arcade.Sprite, Poolable):
     def __init__(self, game):
-        super().__init__("./images/bonus.png")
+        super().__init__()
         self.game = game
         self.scrollcomponent = ScrollComponent(game, self)
 
     def setup(self, row):
+        self.texture = pick_random_texture()
         self.scrollcomponent.setup()
         self.center_y = self.game.y_for_row(row)
 
