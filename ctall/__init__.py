d91acae6d2e1bd9fb70b204fb28a469ab9e1f13b
Aurelien Gateau <mail@agateau.com>
2020-05-27 19:47:55 +0200

    Reorganize code

--- ctall/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for ctall."""
-
-__author__ = """Aurélien Gâteau"""
-__email__ = 'mail@agateau.com'
-__version__ = '0.1.0'
--- ctall/poolable.py
+++
@@ -1,6 +0,0 @@
-class Poolable:
-    def set_pool(self, pool):
-        self._pool = pool
-
-    def recycle(self):
-        self._pool.recycle(self)
