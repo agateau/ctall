6122bd007e85c902e1f139b71a9103c0caf26add
Aurelien Gateau <mail@agateau.com>
2020-06-07 12:04:47 +0200

    Allow loading a mask from a different image

--- src/tools/MaskedTexture.cpp
+++ src/tools/MaskedTexture.cpp
@@ -6,6 +6,18 @@ MaskedTexture::MaskedTexture(Renderer& renderer, const std::string& filename)
         : MaskedTexture(renderer, Surface(filename)) {
 }
 
+MaskedTexture::MaskedTexture(Renderer& renderer,
+                             const std::string& filename,
+                             const std::string& maskFilename)
+        : MaskedTexture(renderer, Surface(filename), Surface(maskFilename)) {
+}
+
 MaskedTexture::MaskedTexture(Renderer& renderer, const SDL2pp::Surface& surface)
-        : texture(renderer, surface), mask(Mask::fromSurfaceAlpha(surface)) {
+        : MaskedTexture(renderer, surface, surface) {
+}
+
+MaskedTexture::MaskedTexture(Renderer& renderer,
+                             const SDL2pp::Surface& surface,
+                             const Surface& surfaceMask)
+        : texture(renderer, surface), mask(Mask::fromSurfaceAlpha(surfaceMask)) {
 }
--- src/tools/MaskedTexture.h
+++ src/tools/MaskedTexture.h
@@ -1,18 +1,21 @@
 #ifndef MASKEDTEXTURE_H
 #define MASKEDTEXTURE_H
 
+#include <SDL2pp/SDL2pp.hh>
 #include <string>
 
 #include "Mask.h"
-
-#include <SDL2pp/SDL2pp.hh>
 
 class Mask;
 
 class MaskedTexture {
 public:
     MaskedTexture(SDL2pp::Renderer& renderer, const std::string& filename);
+    MaskedTexture(SDL2pp::Renderer& renderer, const std::string& filename, const std::string& maskFilename);
     MaskedTexture(SDL2pp::Renderer& renderer, const SDL2pp::Surface& surface);
+    MaskedTexture(SDL2pp::Renderer& renderer,
+                  const SDL2pp::Surface& surface,
+                  const SDL2pp::Surface& surfaceMask);
 
     SDL2pp::Texture texture;
     const Mask mask;
