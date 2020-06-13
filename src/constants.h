412b7f9b8fa09a5ab7a50e0d11283bd4a957da6f
Aurelien Gateau <mail@agateau.com>
2020-06-13 20:20:40 +0200

    Small tweaks

--- src/constants.h
+++ src/constants.h
@@ -8,10 +8,10 @@ static constexpr int SCREEN_SCALE = 4;
 static constexpr int LANE_COUNT = 5;
 static constexpr int TILE_SIZE = 16;
 
-static constexpr float START_SCROLL_SPEED = 200;
+static constexpr float START_SCROLL_SPEED = 300;
 static constexpr int SPAWN_SPACING = 3;
 
-static constexpr float LEVEL_LENGTH = 2000;
-static constexpr float LEVEL_SPEEDUP = 1.1;
+static constexpr float LEVEL_LENGTH = 1000;
+static constexpr float LEVEL_SPEEDUP = 1.1f;
 
 #endif /* CONSTANTS_H */
