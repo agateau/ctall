59080950ac36d28c79e0c3bfc50e17d2c0cc7ef8
Aurelien Gateau <mail@agateau.com>
2020-06-14 19:44:41 +0200

    Switch from .aseprite to .ora files

--- assets-src/generate.sh
+++ assets-src/generate.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 set -euo pipefail
-ASEPRITE=${ASEPRITE:-aseprite}
-
-PLAYER_COUNT=2
 
 cd $(dirname $0)
 
 SRC_DIR=$PWD
+TOOLS_DIR=$PWD
 DST_DIR=$PWD/../assets
 
-for name in player player-down player-up wall ; do
-    echo "Processing $name"
-    cd $DST_DIR
-    $ASEPRITE --batch --split-layers --all-layers $SRC_DIR/$name.aseprite \
-        --filename-format '{title}-{layer}.{extension}' --save-as $name.png
-    if [ -e $name-base.png ] ; then
-        mv $name-base.png $name.png
-    fi
-done
-
-for name in player player-down player-up ; do
-    echo "Post-processing $name"
-    cd $DST_DIR
-    for idx in $(seq 0 $((PLAYER_COUNT-1))) ; do
-        cp $name-mask.png $name-$idx-mask.png
-        mv $name-base-$idx.png $name-$idx.png
+main() {
+    for name in player player-down player-up ; do
+        $TOOLS_DIR/oralayers2png $SRC_DIR/$name.ora \
+            base-0=$DST_DIR/$name-0.png \
+            base-1=$DST_DIR/$name-1.png \
+            mask=$DST_DIR/$name-0-mask.png \
+            mask=$DST_DIR/$name-1-mask.png
     done
-    rm $name-mask.png
-done
+
+    $TOOLS_DIR/oralayers2png $SRC_DIR/wall.ora \
+        base=$DST_DIR/wall.png \
+        mask=$DST_DIR/wall-mask.png
+}
+
+main
